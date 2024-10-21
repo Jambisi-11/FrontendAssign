55ed3cd6b32bd7c4dcd59d4f1f4fc978c87ca2b0
jambisi <jambis.banji@yahoo.com>
2024-10-21 16:06:55 +0100

    slide pictures code added

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
-const slides = document.querySelectorAll('.display img');
-let currentSlide = 0;
-let playing = true;
+let slideIndex = 1;
 
-// Add event listeners for controls
-document.querySelector('.pause').addEventListener('click', () => {
-  playing = false;
-});
-
-document.querySelector('.play').addEventListener('click', () => {
-  playing = true;
-  autoSlide();
-});
-
-document.querySelector('.prev').addEventListener('click', () => {
-  currentSlide--;
-  if (currentSlide < 0) currentSlide = slides.length - 1;
-  updateSlides();
-});
-
-document.querySelector('.next').addEventListener('click', () => {
-  currentSlide++;
-  if (currentSlide >= slides.length) currentSlide = 0;
-  updateSlides();
-});
-
-function autoSlide() {
-  if (playing) {
-    currentSlide++;
-    if (currentSlide >= slides.length) currentSlide = 0;
-    const translateX = -currentSlide * 100;
-    document.querySelector('.slides').style.transform = `translateX(${translateX}%)`;
-    setTimeout(autoSlide, 3000);
-  }
+window.onload = function() {
+  showSlides(slideIndex);
+  // Add this line to start the automatic slideshow
+  setInterval(function() { plusSlides(1); }, 7000); // Change slide every 5 seconds
 }
 
-autoSlide();
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
